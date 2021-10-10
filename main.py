cdab200c200340cb6d2cb63a8f96d45722c4ff7c
Harapecopecopeco <yusuke.watanabe@hopin.co.jp>
2021-10-10 22:49:04 +0900

    :+1:Create index.html for posting additem

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 from flask import Flask, request, render_template
+from datetime import datetime
 
 app = Flask(__name__)
 
 
 @app.route('/', methods=["GET"])
 def hello():
-    information = 0
-    return render_template("index.html", information=information)
+    information = "TODOを入力"
+    dt = datetime.now()
+    return render_template("index.html", information=information, dt=dt)
+
+
+@app.route('/additem', methods=["GET", "POST"])
+def add_item():
+    print(f"Form data: {dict(request.form)}")
+    contents = request.form["contents"]
+    return render_template("index.html", contents=contents)
 
 
 if __name__ == '__main__':
