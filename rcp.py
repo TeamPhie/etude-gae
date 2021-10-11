2c75e1be71a29eebd898c8792a2c761a70d66a51
中津日向 <nakatsuhinata@nakatsuhinatanoMacBook-Pro.local>
2021-10-11 22:59:43 +0900

    rcp_program

---
+++ rcp.py
@@ -0,0 +1,32 @@
+import random
+
+player = input()
+computer = random.randint(0,2)
+
+if player == "Goo":
+    player_number = 0
+    if computer == 0:
+        print("aikoでしょ")
+    elif computer == 1:
+        print("YOU WIN!!")
+    else:
+        print("YOU LOSE..")
+elif player == "Chii":
+    player_number = 1
+    if computer == 0:
+        print("YOU LOSE..")
+    elif computer == 1:
+        print("aikoでしょ")
+    else:
+        print("YOU WIN!!")
+elif player == "Par":
+    player_number = 2
+    if computer == 0:
+        print("YOU WIN!!")
+    elif computer == 1:
+        print("YOU LOSE..")
+    else:
+        print("aikoでしょ")
+else:
+    print("Goo or Chii , Par plese...")
+
---
+++ rcp_renewal.py
@@ -0,0 +1,17 @@
+import random
+same_list = [[0, 0], [1, 1], [2, 2]]
+win_list = [[0, 1], [1, 2], [2, 0]]
+lose_list = [[0, 2], [1, 0], [2, 1]]
+computer = random.randint(0, 2)
+player = input()
+if player == "Goo":
+    player_number = 0
+elif player == "Chii":
+    player_number = 1
+elif player == "Par":
+    player_number = 2
+else:
+    print("Goo or Chii , Par plese...")
+
+rcp_list = [player, computer]
+
